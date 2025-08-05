0caaad152ff83436896aada831944e6e599e6671
wu-yiqiang <wu_yiqiang@outlook.com>
2025-08-06 02:25:35 +0800

    112

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 class HomePage extends StatelessWidget {
+  List<Container> cards = [
+    Container(
+      alignment: Alignment.center,
+      child: const Text('1'),
+      color: Colors.blue,
+    ),
+    Container(
+      alignment: Alignment.center,
+      child: const Text('2'),
+      color: Colors.red,
+    ),
+    Container(
+      alignment: Alignment.center,
+      child: const Text('3'),
+      color: Colors.purple,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Container(
-              width: 120,
-              height: 120,
-              margin: EdgeInsets.only(top: 30, bottom: 30),
-              child: Text("Home"),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 200,
+                    child: Swiper(
+                      itemBuilder: (BuildContext context, int index) {
+                        final int inde = index + 1;
+                        return Image.asset(
+                          "assets/images/background$inde.png",
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      itemCount: 3,
+                      pagination: SwiperPagination(),
+                      control: SwiperControl(),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
--- lib/views/workbench/workbench.dart
+++ lib/views/workbench/workbench.dart
@@ -2,21 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class WorkbenchPage extends StatelessWidget {
+  late TabController _tabController;
+  List tabs = ["设备","人员", "已完成"];
+  _tabController = TabController(length: tabs.length, vsync: this);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              width: 120,
-              height: 120,
-              margin: EdgeInsets.only(top: 30, bottom: 30),
-              child: Text("WorkbenchPage"),
-            ),
-          ],
+        child: TabBarView(
+          controller: _tabController,
+          children: tabs.map((e) {
+            return Text("sdsd");
+          }).toList(),
         ),
       ),
     );
   }
+  @override
+  void initState() {
+    // super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+  }
+
 }
