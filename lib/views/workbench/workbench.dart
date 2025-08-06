68e7002857b043af050b79ac40869c3d7c00293c
wu-yiqiang <wu_yiqiang@outlook.com>
2025-08-07 02:24:54 +0800

    添加tabs

--- lib/views/workbench/workbench.dart
+++ lib/views/workbench/workbench.dart
@@ -2,26 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class WorkbenchPage extends StatelessWidget {
-  late TabController _tabController;
-  List tabs = ["设备","人员", "已完成"];
-  _tabController = TabController(length: tabs.length, vsync: this);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: TabBarView(
-          controller: _tabController,
-          children: tabs.map((e) {
-            return Text("sdsd");
-          }).toList(),
+    final List<Tab> myTabs = <Tab>[
+      Tab(text: '待审批'),
+      Tab(text: '审批中'),
+      Tab(text: '已审批'),
+      Tab(text: '已完成'),
+      Tab(text: '11'),
+      Tab(text: '221'),
+      Tab(text: '781'),
+    ];
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Menus"),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.update)),
+          ],
+          bottom: TabBar(tabs: myTabs),
+        ),
+        body: SafeArea(
+          child: TabBarView(
+            children: <Widget>[
+              Center(child: Text('Tab 1')),
+              Center(child: Text('Tab 2')),
+              Center(child: Text('Tab 3')),
+              Center(child: Text('Tab 4')),
+              Center(child: Text('萨达')),
+              Center(child: Text('萨达111')),
+              Center(child: Text('萨达222')),
+            ],
+          ),
         ),
       ),
     );
+    // return Scaffold(
+    //   appBar: AppBar(bottom: TabBar(tabs: myTabs)),
+    //   body: SafeArea(
+    //     child: TabBarView(
+    //       children: <Widget>[
+    //         Center(child: Text('Tab 1')),
+    //         Center(child: Text('Tab 2')),
+    //         Center(child: Text('Tab 3')),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
-  @override
-  void initState() {
-    // super.initState();
-    _tabController = TabController(length: tabs.length, vsync: this);
-  }
-
 }
